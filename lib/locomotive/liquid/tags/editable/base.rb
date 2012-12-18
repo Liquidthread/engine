d4c99e6a03ab6be91e720ae252a81f1d05c4c46f
Ryan Messner <rpmessner@gmail.com>
2012-12-18 15:47:01 -0600

    revert

--- lib/locomotive/liquid/tags/editable/base.rb
+++ lib/locomotive/liquid/tags/editable/base.rb
@@ -68,14 +68,7 @@ module Locomotive
 
           def render_default_content(context)
             begin
-              response = render_all(@nodelist, context)
-              if response.respond_to?(:join)
-                response.join(' ')
-              elsif response.is_a?(String)
-                response
-              else
-                raise Exception.new
-              end
+              render_all(@nodelist, context).join(' ')
             rescue
               raise ::Liquid::SyntaxError.new("Error in the #{self.current_block_name || 'default'} block for the #{@slug} editable_element - No liquid tags are allowed inside.")
             end
